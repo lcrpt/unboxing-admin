516c9bc1e068bb4ba6cd74e689bd197c77a9dd35
leolassence <leolassence@gmail.com>
2016-11-29 14:00:13 +0100

    setup project with express node server and angular

---
+++ app/js/app.js
@@ -0,0 +1,1 @@
+angular.module('unboxingAdmin', ['ngRoute', 'appRoutes', 'MainCtrl']);
---
+++ app/js/controllers/MainCtrl.js
@@ -0,0 +1,3 @@
+angular.module('MainCtrl', []).controller('MainController', function($scope) {
+	$scope.tagline = 'Select an entity to manage';
+});
